7af0fe908bd801fc44536323dd9c480309d5a568
esmail <smail.anonymous8@gmail.com>
2024-06-05 01:17:44 +0200

    fix final

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -3,15 +3,10 @@ import { BookingController } from './booking.controller';
 import { BookingService } from './booking.service';
 import { PrismaService } from 'src/prisma.service';
 import { NotificationService } from 'src/notification/notification.service';
-import { NotificationGateway } from 'src/notification/notification.gateway';
+//import { NotificationGateway } from 'src/notification/notification.gateway';
 
 @Module({
   controllers: [BookingController],
-  providers: [
-    BookingService,
-    PrismaService,
-    NotificationService,
-    NotificationGateway,
-  ],
+  providers: [BookingService, PrismaService, NotificationService],
 })
 export class BookingModule {}
